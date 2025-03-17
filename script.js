3ec9595c4223571dfd0ba9513be68c173a73b21b
Gordon Sheng <shengzixiang08@gmail.com>
2025-03-17 17:12:03 -0500

    fixed service workier not being registered bug

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/service-worker.js')
+      navigator.serviceWorker.register('/serviceWorker.js')
         .then(registration => console.log('Service Worker registered:', registration.scope))
         .catch(error => console.error('Service Worker registration failed:', error));
     });
